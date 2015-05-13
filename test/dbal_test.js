c72aa6255da6f81793df7aaa48f0dab50d8d969c
Ketil Albertsen <ketil@entitet.no>
2015-05-13 14:31:49 +0200

    Revert to direct fetch methods

--- test/dbal_test.js
+++ test/dbal_test.js
@@ -3,34 +3,42 @@ var expect = require('chai').expect;
 var Dbal = require('../lib/dbal');
 
 describe('Dbal', function() {
-  describe('.acquire', function() {
-    it('acquires pooled client', function() {
-      var dbal = Dbal();
-      var client = dbal.acquire();
-      client.connect().then(function() {
-        var count = Object.keys(dbal.pg.pools.all).length;
-        expect(count).equal(1);
-        client.end();
-        expect(count).equal(0);
-      });
-    });
+  var dbal;
+
+  beforeEach(function() {
+    dbal = Dbal();
+    dbal.pg.end();
   });
 
   describe('.client', function() {
-    it('acquires standalone client', function() {
-      var dbal = Dbal();
+    it('acquires standalone client', function(done) {
       var client = dbal.client();
       client.connect().then(function() {
-        var count = Object.keys(dbal.pg.pools.all).length;
-        expect(count).equal(0);
+        var pool = dbal.pg.pools.all;
+        expect(Object.keys(pool)).length(0);
         client.end();
-      });
+        done();
+      }).catch(done);
+    });
+  });
+
+  describe('.acquire', function() {
+    it('acquires pooled client', function(done) {
+      var client = dbal.acquire();
+      client.connect().then(function() {
+        var pool = dbal.pg.pools.all;
+        expect(Object.keys(pool)).length(1);
+        client.end();
+        expect(Object.keys(pool)).length(1);
+        dbal.end();
+        expect(Object.keys(pool)).length(0);
+        done();
+      }).catch(done);
     });
   });
 
   describe('.run', function() {
     it('runs query returning to pool', function(done) {
-      var dbal = Dbal();
       dbal.run('SELECT 1').then(function(res) {
         expect(res.rowCount).equal(1);
         done();
@@ -38,38 +46,25 @@ describe('Dbal', function() {
     });
 
     it('throws on error', function(done) {
-      var dbal = Dbal();
       dbal.run('SELECT * FROM "nonexistant"').then(done).catch(function(err) {
         expect(err.code).equal('42P01');
         done();
       });
     });
+  });
 
-    describe('promise', function() {
-      it('.all returns all rows', function(done) {
-        var dbal = Dbal();
-        dbal.run('SELECT * FROM generate_series(1,5)').all().then(function(rows) {
-          expect(rows).instanceof(Array).length(5);
-          done();
-        }).catch(done);
-      });
+  it('.all returns all rows', function(done) {
+    dbal.all('SELECT * FROM generate_series(1,5)').then(function(rows) {
+      expect(rows).instanceof(Array).length(5);
+      done();
+    }).catch(done);
+  });
 
-      it('.one returns first row', function(done) {
-        var dbal = Dbal();
-        dbal.run("SELECT 'foo' as bar").one().then(function(row) {
-          expect(row).all.keys('bar');
-          expect(row.bar).equal('foo');
-          done();
-        }).catch(done);
-      });
-
-      it('.count returns rowCount', function(done) {
-        var dbal = Dbal();
-        dbal.run('SELECT * FROM generate_series(1,3)').count().then(function(count) {
-          expect(count).equal(3);
-          done();
-        }).catch(done);
-      });
-    });
+  it('.one returns first row', function(done) {
+    dbal.one("SELECT 'foo' as bar").then(function(row) {
+      expect(row).all.keys('bar');
+      expect(row.bar).equal('foo');
+      done();
+    }).catch(done);
   });
 });
