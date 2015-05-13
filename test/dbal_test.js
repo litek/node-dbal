1b4c366cbd95260c3717cc640ea95485398e61f2
Ketil Albertsen <ketil@entitet.no>
2015-05-13 13:58:27 +0200

    Add .count to promise

--- test/dbal_test.js
+++ test/dbal_test.js
@@ -45,21 +45,31 @@ describe('Dbal', function() {
       });
     });
 
-    it('attaches .all promise', function(done) {
-      var dbal = Dbal();
-      dbal.run('SELECT * FROM generate_series(1,5)').all().then(function(rows) {
-        expect(rows).instanceof(Array).length(5);
-        done();
-      }).catch(done);
-    });
+    describe('promise', function() {
+      it('.all returns all rows', function(done) {
+        var dbal = Dbal();
+        dbal.run('SELECT * FROM generate_series(1,5)').all().then(function(rows) {
+          expect(rows).instanceof(Array).length(5);
+          done();
+        }).catch(done);
+      });
 
-    it('attaches .one promise', function(done) {
-      var dbal = Dbal();
-      dbal.run("SELECT 'foo' as bar").one().then(function(row) {
-        expect(row).all.keys('bar');
-        expect(row.bar).equal('foo');
-        done();
-      }).catch(done);
+      it('.one returns first row', function(done) {
+        var dbal = Dbal();
+        dbal.run("SELECT 'foo' as bar").one().then(function(row) {
+          expect(row).all.keys('bar');
+          expect(row.bar).equal('foo');
+          done();
+        }).catch(done);
+      });
+
+      it('.count returns rowCount', function(done) {
+        var dbal = Dbal();
+        dbal.run('SELECT * FROM generate_series(1,3)').count().then(function(count) {
+          expect(count).equal(3);
+          done();
+        }).catch(done);
+      });
     });
   });
 });
