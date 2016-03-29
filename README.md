1837a78feb73723edf9e925f5e0e67306ed203d9
litek <ketil@entitet.no>
2016-03-29 20:04:28 +0200

    Simplify connection wrapper

--- README.md
+++ README.md
@@ -2,5 +2,14 @@
 
 Simple wrapper on top of [pg](https://github.com/brianc/node-postgres) and [sql](https://github.com/brianc/node-sql).
 
-## Version 0.3
-Library has been mostly rewritten. Only promises are now supported.
+```javascript
+'use strict'
+const Dbal = require('dbal')
+const db = new Dbal(process.env.DBAL_URL)
+
+db.table('users').insert({name: 'Foo'}).returning('id', 'created', 'name').one().then(function(res) {
+  console.log('Inserted new user: ' + JSON.stringify(res))
+}).catch(function(err) {
+  console.log(err.stack)
+})
+```
--- test/dbal_test.js
+++ test/dbal_test.js
@@ -1,84 +1,25 @@
-'use strict';
-var expect = require('chai').expect;
-var dbal = require('../lib/dbal');
+'use strict'
+const Connection = require('../lib/connection')
 
 describe('dbal', function() {
-  var db;
-
   beforeEach(function() {
-    db = dbal('postgres://localhost/postgres');
-    db.pg.end();
-  });
+    let url = process.env.DBAL_URL || 'postgres://localhost/dbal_test'
+    let db = this.db = new Connection(url)
 
-  describe('.table', function() {
-    it('adds columns dynamically', function() {
-      var users = db.define({name: 'users', columns: ['id']});
-      expect(users.id).an('object');
-      db.define('users', ['email']);
-      expect(users.email).an('object');
-    });
+    return db.run('DROP TABLE IF EXISTS test').then(function() {
+      return db.run('CREATE TABLE test (id integer not null, name text not null)')
+    }).then(function() {
+      return db.run("INSERT INTO test VALUES (1, 'Foo'), (2, 'Bar'), (3, 'Baz')")
+    })
+  })
 
-    it('callable is an alias for .table', function() {
-      var users = db({name: 'users', columns: ['id']});
-      expect(users.id).an('object');
-    });
-  });
+  afterEach(function() {
+    this.db.run('DROP TABLE test').then(function() {
+      return this.db.end()
+    }.bind(this))
+  })
 
-  describe('.client', function() {
-    it('acquires standalone client', function(done) {
-      var client = db.client();
-      client.connect().then(function() {
-        var pool = db.pg.pools.all;
-        expect(Object.keys(pool)).length(0);
-        client.end();
-        done();
-      }).catch(done);
-    });
-  });
-
-  describe('.acquire', function() {
-    it('acquires pooled client', function(done) {
-      var client = db.acquire();
-      client.connect().then(function() {
-        var pool = db.pg.pools.all;
-        expect(Object.keys(pool)).length(1);
-        client.end();
-        expect(Object.keys(pool)).length(1);
-        db.end();
-        expect(Object.keys(pool)).length(0);
-        done();
-      }).catch(done);
-    });
-  });
-
-  describe('.run', function() {
-    it('runs query returning to pool', function(done) {
-      db.run('SELECT 1').then(function(res) {
-        expect(res.rowCount).equal(1);
-        done();
-      }).catch(done);
-    });
-
-    it('throws on error', function(done) {
-      db.run('SELECT * FROM "nonexistant"').then(done).catch(function(err) {
-        expect(err.code).equal('42P01');
-        done();
-      });
-    });
-  });
-
-  it('.all returns all rows', function(done) {
-    db.all('SELECT * FROM generate_series(1,5)').then(function(rows) {
-      expect(rows).instanceof(Array).length(5);
-      done();
-    }).catch(done);
-  });
-
-  it('.one returns first row', function(done) {
-    db.one("SELECT 'foo' as bar").then(function(row) {
-      expect(row).all.keys('bar');
-      expect(row.bar).equal('foo');
-      done();
-    }).catch(done);
-  });
-});
+  require('./lib/connection_test')
+  require('./lib/client_test')
+  require('./lib/node_test')
+})
